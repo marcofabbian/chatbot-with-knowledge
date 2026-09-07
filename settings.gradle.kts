a128d5ed414b33b14df85f23aacdef3ae4e4e151
Marco Fabbian <marco.fabbian@gmail.com>
2026-09-08 00:00:04 +0200

    Add web controller

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+plugins {
+    // Allows Gradle to auto-download JDK toolchains (e.g. JDK 20) when not installed locally
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "chatbot-with-knowledge"
